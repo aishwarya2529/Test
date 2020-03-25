197cd076f1b7f4c85bef83f74d66967c79860b27
aishwarya2529 <62608673+aishwarya2529@users.noreply.github.com>
2020-03-25 20:42:32 +0530

    Add files via upload

---
+++ src/main/java/io/springbootstarter/topic/TopicReository.java
@@ -0,0 +1,9 @@
+package io.springbootstarter.topic;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TopicReository extends CrudRepository<Topic, String> {
+	
+	
+
+}
